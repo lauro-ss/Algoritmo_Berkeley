f94621f8001ec443b9db86e53b9a188369e10e5d
Lauro Santana Silva <bestlaurobr@gmail.com>
2023-03-04 13:41:26 -0300

    Coletando qtd de servidores e enviando hora

--- src/PrimaryClock.java
+++ src/PrimaryClock.java
@@ -1,7 +1,43 @@
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketTimeoutException;
+
 public class PrimaryClock {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int secondarysServers = 0;
+        // Classe java para trabalhar com multicast ou broadcast
+        MulticastSocket mcs = new MulticastSocket(6000);// porta como parametro
+        // Endere�o de um grupo multicast
+        InetAddress grp = InetAddress.getByName("239.0.0.1");
+        // ingressando em um grupo para receber mensagens enviadas para o mesmo
+        mcs.joinGroup(grp);
+
+        // Recebe a quantidade de relógios secundários
+        try {
+            byte rec[] = new byte[256];
+            DatagramPacket pkg = new DatagramPacket(rec, rec.length);
+
+            // Espera 5 segundos pros relógios secundários conectar
+            mcs.setSoTimeout(5000);
+            while (true) {
+                mcs.receive(pkg);// recebendo os dados enviados via multicast para o endere�o acima
+                if (pkg.getData().length > 0) {
+                    secondarysServers++;
+                }
+            }
+        } catch (SocketTimeoutException te) {
+            System.out.println("Acabou o tempo de respostas, número e servidores: " + secondarysServers);
+            byte[] hora = "Hora".getBytes();
+            DatagramPacket pkgHora = new DatagramPacket(hora, hora.length, InetAddress.getByName("239.0.0.1"),
+                    6000);
+            mcs.send(pkgHora);
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
     }
-    
+
 }
--- src/SecondaryClock.java
+++ src/SecondaryClock.java
@@ -1,5 +1,32 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class SecondaryClock {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        try {
+            byte[] b = "Conectando".getBytes();
+            // Definindo o endere�o de envio do pacote neste caso o endere�o de multicast
+            InetAddress addr = InetAddress.getByName("239.0.0.1");
+            DatagramPacket pkg = new DatagramPacket(b, b.length, addr, 6000);
+            DatagramSocket ds = new DatagramSocket();
+            ds.send(pkg);// enviando pacote multicast
+
+            // Classe java para trabalhar com multicast ou broadcast
+            MulticastSocket mcs = new MulticastSocket(6000);// porta como parametro
+            // Endere�o de um grupo multicast
+            InetAddress grp = InetAddress.getByName("239.0.0.1");
+            // ingressando em um grupo para receber mensagens enviadas para o mesmo
+            mcs.joinGroup(grp);
+            while (true) {
+                mcs.receive(pkg);
+                String t = new String(pkg.getData(), 0, pkg.getLength());
+                System.out.println(t);
+            }
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel enviar a mensagem");
+        }
     }
 }
