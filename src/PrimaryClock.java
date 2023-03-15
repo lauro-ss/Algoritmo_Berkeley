5be61a54cdf2275772bdf884199f2a327bfb7258
Lauro Santana Silva <bestlaurobr@gmail.com>
2023-03-15 20:46:56 -0300

    Add thread main

--- src/PrimaryClock.java
+++ src/PrimaryClock.java
@@ -6,53 +6,65 @@ import java.net.SocketTimeoutException;
 import java.sql.Time;
 import java.util.ArrayList;
 
-public class PrimaryClock {
+public class PrimaryClock extends Thread {
 
-    public static void main(String[] args) throws Exception {
+    private int id;
+    private Time hora;
+    private ArrayList<Long> horariosServidores;
+
+    public PrimaryClock(int id, Time hora) {
+        this.id = id;
+        this.hora = hora;
+        horariosServidores = new ArrayList<Long>();
+    }
+
+    public void run() {
 
         int secondarysServers = 1;
-        Time hora = Time.valueOf("03:00:00");
-        ArrayList<Long> horariosServidores = new ArrayList<Long>();
 
-        MulticastSocket mcs = new MulticastSocket(6000);
-        InetAddress grp = InetAddress.getByName("239.0.0.1");
-        mcs.joinGroup(grp);
+        try (MulticastSocket mcs = new MulticastSocket(6000)) {
+            InetAddress grp = InetAddress.getByName("239.0.0.1");
+            mcs.joinGroup(grp);
 
-        try {
-            byte rec[] = new byte[256];
-            DatagramPacket pkg = new DatagramPacket(rec, rec.length);
+            try {
+                byte rec[] = new byte[256];
+                DatagramPacket pkg = new DatagramPacket(rec, rec.length);
 
-            // Espera 5 segundos pros relógios secundários conectar
-            mcs.setSoTimeout(5000);
-            while (true) {
-                mcs.receive(pkg);
-                if (pkg.getData().length > 0) {
-                    secondarysServers++;
+                // Espera 5 segundos pros relógios secundários conectar
+                mcs.setSoTimeout(5000);
+                while (true) {
+                    mcs.receive(pkg);
+                    if (pkg.getData().length > 0) {
+                        secondarysServers++;
+                    }
                 }
+            } catch (SocketTimeoutException te) {
+                EnviarHora(hora, mcs);
+
+                ReceberHoras(horariosServidores, mcs);
+
+                Long totalTime = (long) 0;
+                for (Long time : horariosServidores) {
+                    totalTime += time;
+                }
+
+                hora.setTime(hora.getTime() + (totalTime / (horariosServidores.size() + 1)));
+
+                EnviarHora(hora, mcs);
+
+                System.out.println("Novo horário servidor primário: " + hora.toString());
+
+            } catch (Exception e) {
+                System.out.println("Erro: " + e.getMessage());
             }
-        } catch (SocketTimeoutException te) {
-            EnviarHora(hora, mcs);
-
-            ReceberHoras(horariosServidores, mcs);
-
-            Long totalTime = (long) 0;
-            for (Long time : horariosServidores) {
-                totalTime += time;
-            }
-
-            hora.setTime(hora.getTime() + (totalTime / secondarysServers));
-
-            EnviarHora(hora, mcs);
-
-            System.out.println("Novo horário servidor primário: " + hora.toString());
-
-        } catch (Exception e) {
-            System.out.println("Erro: " + e.getMessage());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
     }
 
-    private static void EnviarHora(Time hora, MulticastSocket mcs) throws IOException {
+    private void EnviarHora(Time hora, MulticastSocket mcs) throws IOException {
         byte[] horaByte = hora.toString().getBytes();
         DatagramPacket pkgHora = new DatagramPacket(horaByte, horaByte.length,
                 InetAddress.getByName("239.0.0.1"),
@@ -60,7 +72,7 @@ public class PrimaryClock {
         mcs.send(pkgHora);
     }
 
-    private static void ReceberHoras(ArrayList<Long> horariosServidores, MulticastSocket mcs) throws IOException {
+    private void ReceberHoras(ArrayList<Long> horariosServidores, MulticastSocket mcs) throws IOException {
         byte rec[] = new byte[256];
         DatagramPacket pkg = new DatagramPacket(rec, rec.length);
         try {
--- src/RunClocks.java
+++ src/RunClocks.java
@@ -7,13 +7,17 @@ public class RunClocks {
 
         // Simulando exemplo do slide
         if (exemploSlide) {
+            new PrimaryClock(1, Time.valueOf("03:00:00")).start();
+
             new SecondaryClock(2, Time.valueOf("02:50:00")).start();
             new SecondaryClock(3, Time.valueOf("03:25:00")).start();
         } else {
+            new PrimaryClock(1, Time.valueOf("03:00:00")).start();
+
             // 60 / 4 = 15 ~ esperado = 03:15:00
             new SecondaryClock(2, Time.valueOf("03:20:00")).start();
             new SecondaryClock(3, Time.valueOf("03:20:00")).start();
-            new SecondaryClock(3, Time.valueOf("03:20:00")).start();
+            new SecondaryClock(4, Time.valueOf("03:20:00")).start();
         }
     }
 }
